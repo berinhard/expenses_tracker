deb0d95cc03ceaf64f87f81faaf0ee81d9eb7164
Bernardo Fontes <bernardoxhc@gmail.com>
2015-06-01 09:00:17 -0300

    Adds main app skeleton

--- project/src/settings.py
+++ project/src/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'src.expenses',
     'debug_toolbar',
     'django_extensions',
 )
