1bc611fb76f8d6f89e5da08118cf5d6cd08f53b7
Bernardo Fontes <bernardoxhc@gmail.com>
2015-06-01 11:52:48 -0300

    Sends url resource on data for retrieved expenses

--- project/src/expenses/tests/test_serializers.py
+++ project/src/expenses/tests/test_serializers.py
@@ -2,6 +2,7 @@
 from decimal import Decimal
 from datetime import date
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from src.expenses.models import Expense
@@ -21,6 +22,7 @@ class TestExpenseSerializer(TestCase):
         )
 
         serializer = ExpenseSerializer(expense)
+        resource_url = reverse('expenses:expense_detail', args=[42])
 
         self.assertEqual(42, serializer.data['id'])
         self.assertEqual(u'10.00', serializer.data['value'])
@@ -28,6 +30,7 @@ class TestExpenseSerializer(TestCase):
         self.assertEqual(category[1], serializer.data['category_display'])
         self.assertEqual('2015-01-01', serializer.data['date'])
         self.assertEqual('foo', serializer.data['description'])
+        self.assertEqual(resource_url, serializer.data['resource_url'])
 
     def test_creates_expense(self):
         data = {
