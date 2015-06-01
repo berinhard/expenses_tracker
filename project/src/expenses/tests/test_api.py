deb8bba592138ed09e4e1f107f5c7c11b19677f3
Bernardo Fontes <bernardoxhc@gmail.com>
2015-06-01 09:50:41 -0300

    Adds tests to ensure generic view configuration

--- project/src/expenses/tests/test_api.py
+++ project/src/expenses/tests/test_api.py
@@ -5,7 +5,9 @@ from model_mommy import mommy
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
+from src.expenses.api import ListExpensesAPI
 from src.expenses.models import Expense
+from src.expenses.serializers import ExpenseSerializer
 
 class TestExpensesListApiMethod(TestCase):
 
@@ -23,3 +25,11 @@ class TestExpensesListApiMethod(TestCase):
         expected_keys = ['id', 'value', 'category', 'category_display', 'date', 'description']
         for key in expected_keys:
             self.assertIn(key, content[0])
+
+    def test_ensure_generic_view_configuration(self):
+        mommy.make(Expense)
+
+        qs = Expense.objects.all()
+
+        self.assertEqual(list(qs), list(ListExpensesAPI.queryset))
+        self.assertEqual(ExpenseSerializer, ListExpensesAPI.serializer_class)
